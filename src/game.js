e1a944ec017185f509bd1d35ad50a156b205a409
gecendemarchi_vwi <gecendemarchi@vivaweb.net>
2020-04-10 11:17:03 -0300

    Joguinho para estudos

---
+++ src/game.js
@@ -0,0 +1,101 @@
+import  React from "react";
+import calculateWinner from "./calculateWinner";
+import  Board from "./board";
+
+class Game extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            history: [
+                {
+                    squares: Array(9).fill(null)
+                }
+            ],
+            xIsNext: true,
+            stepNumber: 0,
+        };
+    }
+
+    handleClick(i) {
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
+        const current = history[history.length - 1];
+        const squares = current.squares.slice();
+        if (calculateWinner(squares) || squares[i])
+        {
+            return;
+        }
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState(
+            {
+                history: history.concat([
+                    {
+                        squares: squares
+                    }
+                ]),
+                xIsNext: !this.state.xIsNext,
+                stepNumber: history.length
+            }
+        );
+    }
+
+    jumpTo(step) {
+        if (!step)
+        {
+            this.setState(
+                {
+                    history: [
+                        {
+                            squares: Array(9).fill(null)
+                        }
+                    ],
+                    xIsNext: true,
+                    stepNumber: 0,
+                }
+            );
+        }
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step % 2) === 0,
+        });
+    }
+
+    render() {
+        const history = this.state.history;
+        const current = history[this.state.stepNumber];
+        const winner = calculateWinner(current.squares);
+
+        const moves = history.map((step, move) => {
+            const desc = move ? 'Vai para o movimento#' + move : 'Reset - Iniciar';
+            return (
+                <li>
+                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                </li>
+            );
+        });
+
+        let status;
+        if (winner)
+        {
+            status = 'Vencedor: ' + winner;
+        } else
+        {
+            status = 'Próximo jogador: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board
+                        squares={current.squares}
+                        onClick={(i) => this.handleClick(i)}
+                    />
+                </div>
+                <div className="game-info">
+                    <div>{status}</div>
+                    <ol>{moves}</ol>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Game;
